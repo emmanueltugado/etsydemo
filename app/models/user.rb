d24b2a47daf503f1e6676a47a1544981a882ce60
Emmanuel Tugado <emmanueltugado@gmail.com>
2014-05-29 21:34:45 +0800

    Enable users to place orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   has_many :listings, dependent: :destroy
+
+  # use the seller_id column in Order table
+  has_many :sales, class_name: "Order", foreign_key: "seller_id"
+  
+  # use the buyer_id column in Order table
+  has_many :purchases, class_name: "Order", foreign_key: "buyer_id"
 end
